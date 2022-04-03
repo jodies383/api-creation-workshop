e7033d45b37da5f02557de95987fd080d9081075
Jodie Solomons <jodies383@gmail.com>
2022-04-03 16:54:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// enable the static folder...
+app.use(express.static('public'));
+
+// import the dataset to be used here
+
+const PORT = process.env.PORT || 4017;
+
+// API routes to be added here
+
+app.listen(PORT, function() {
+	console.log(`App started on port ${PORT}`)
+});
