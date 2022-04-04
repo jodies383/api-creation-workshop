deff4a7363c2db69f1242207c121e264a5c1c051
Jodie Solomons <jodies383@gmail.com>
2022-04-04 14:32:00 +0200

    heroku

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const express = require('express');
+const { get } = require('express/lib/request');
 const app = express();
 
 // enable the static folder...
 app.use(express.static('public'));
-
+// enable the req.body object
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // import the dataset to be used here
+const garments = require('./garments.json');
 
 const PORT = process.env.PORT || 4017;
 
 // API routes to be added here
+app.get('/api/garments', function (req, res) {
+	const gender = req.query.gender;
+	const season = req.query.season;
 
-app.listen(PORT, function() {
+	const filteredGarments = garments.filter(garment => {
+		// if both gender & season was supplied
+		if (gender != 'All' && season != 'All') {
+			return garment.gender === gender
+				&& garment.season === season;
+		} else if (gender != 'All') { // if gender was supplied
+			return garment.gender === gender
+		} else if (season != 'All') { // if season was supplied
+			return garment.season === season
+		}
+		return true;
+	});
+	// note that this route just send JSON data to the browser
+	// there is no template
+	res.json({ garments: filteredGarments });
+});
+app.get('/api/garments/price/:price', function(req, res){
+	const maxPrice = Number(req.params.price);
+	const filteredGarments = garments.filter( garment => {
+		// filter only if the maxPrice is bigger than maxPrice
+		if (maxPrice > 0) {
+			return garment.price <= maxPrice;
+		}
+		return true;
+	});
+
+	res.json({ 
+		garments : filteredGarments
+	});
+});
+app.listen(PORT, function () {
 	console.log(`App started on port ${PORT}`)
 });
 
